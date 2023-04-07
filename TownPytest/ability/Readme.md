d0d5328565154204fc883ddfed030c13c780fa91
unknown <hongzc99@foxmail.com>
2023-04-07 23:04:08 +0800

    prj structure build[first]

---
+++ TownPytest/ability/Readme.md
@@ -0,0 +1,7 @@
+## 集成自动化能力扩展框架
+
+### 1.接口测试：postman/request
+
+### 2.UI测试：Appium/uiautomator2
+
+### 3.抓包测试
---
+++ TownPytest/scripts/test_base_001.py
@@ -0,0 +1,7 @@
+import pytest
+
+class Test_Base_001():
+    def test_base_001(self):
+        a = 21
+        b = 1
+        assert a+b==22
